695a5b01121acd78c736bfd507c07d1cd33a63b2
danishabu007 <abudanish027@gmail.com>
2026-02-13 21:45:23 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+//import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+
 import Navbar from "./components/Navbar";
 import Dashboard from "./components/Dashboard";
 import AddPayment from "./components/AddPayment";
