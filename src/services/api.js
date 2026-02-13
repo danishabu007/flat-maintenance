e21b8f21b552b2a03803547485275b351387b870
danishabu007 <abudanish027@gmail.com>
2026-02-13 20:38:49 +0530

    first

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api" // backend baad me connect karenge
+  baseURL: "https://flat-maintenance-backend.onrender.com/api"
 });
 
 export default API;
